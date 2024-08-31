8db98e7ef47558b6bc076ccadc89c5c617fa9839
Hendrius Félix <hendriusfelix@gmail.com>
2024-08-31 12:50:32 -0300

    server/client

--- server/app.js
+++ server/app.js
@@ -1,3 +1,15 @@
-console.log("Ola val");
+const express = require("express");
+const cors = require("cors");
 
-console.log("Adevaldo chupa cus");
+const app = express();
+const port = 2399;
+
+app.use(cors());
+
+app.listen(port, () => {
+  console.log("Server listening on port: ", port);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
